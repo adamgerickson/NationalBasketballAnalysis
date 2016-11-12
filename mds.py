0d441b7eaf150e8e42985c7fcbd5068155d5c561
dherg <hergendavid@gmail.com>
2016-11-12 16:27:17 -0500

    mds init

---
+++ mds.py
@@ -0,0 +1,11 @@
+# Multi-dimensional Scaling analyis of NBA data
+
+import numpy as np
+
+# read in data from csv to structured numpy array
+data = np.genfromtxt('Data/stats.csv', delimiter=',', dtype=None)
+
+
+
+
+
